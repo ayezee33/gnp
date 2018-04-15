fb887febd81bd01eb82712a12840b43f7f2849d0
Alex Zerbach <ayezee33@gmail.com>
2018-04-14 20:11:28 -0700

    remove buttons and add forms

--- wp-content/themes/sage/template-home.php
+++ wp-content/themes/sage/template-home.php
@@ -12,17 +12,16 @@
       <img class="streamline-illustration center mb2" src="http://growthandpurpose.com/wp-content/uploads/illustration@2x.png" alt="launch idea illustration">
        <h2 class="center mb1">Join Me On A Quest Of Understanding</h2>
        <p class="lead center">What is your life's purpose? It's not an easy question to answer. Start by working through some excercies to get more clarity on where you are going in life.</p>
-       <a class="btn btn-outline btn-center" href="#start">Get Started</a>
       </div>
    </div>
    <div class="row section-break">
      <div class="col-sm-12 col-lg-6">
        <h2 class="mb1">Do You Feel Stuck In Life?</h2>
        <p class="lead">It's okay! It happens to all of us. No matter what part of your life you are struggling with it is up to you to pull yourself out. Click the start button below to read some helpful information about personal growth and development and how to get back on track with your life's purpose.</p>
-       <a class="btn btn-success btn-lg" href="#start">Get Back On Track Today!</a>
+       <?php echo do_shortcode('[gravityform id="1" title="false" description="false"]'); ?>
      </div>
      <div class="col-sm-12 col-lg-6">
-       <img class="streamline-illustration center mb2" src="http://growthandpurpose.com/wp-content/uploads/drowning-illustration@2x.png" alt="drowning illustration">
+       <img class="xl-streamline-illustration center mt2" src="http://growthandpurpose.com/wp-content/uploads/drowning-illustration@2x.png" alt="drowning illustration">
     </div>
    </div>
    <div class="row">
