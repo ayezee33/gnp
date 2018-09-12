b13b46380a42129fe901f408f48c680806550703
Alex Zerbach <alex.zerbach@gmail.com>
2018-09-12 15:59:13 -0700

    Make home page more robust with content

--- wp-content/themes/sage/base.php
+++ wp-content/themes/sage/base.php
@@ -27,14 +27,82 @@ use Roots\Sage\Wrapper;
           <div class="col-sm-12 col-lg-6">
             <h1><strong><?php the_field('hero_heading'); ?></strong></h1>
             <h3 class="lead"><?php the_field('hero_subheading'); ?></h3>
-            <a class="btn btn-lg btn-success" href="/resources/">Get Started</a>
+            <a class="btn btn-lg btn-success" href="/resources/">Get Started Today!</a>
           </div>
           <div class="col-sm-12 col-lg-6">
-            <img class="streamline-illustration center hidden-sm hidden-xs" src="https://growthandpurpose.com/wp-content/uploads/business-checklist.png" alt="user illustration">
+            <img class="streamline-illustration center hidden-sm hidden-xs" src="https://growthandpurpose.com/wp-content/uploads/20-business-target-checklist.svg" alt="goal checklist illustration">
           </div>
         </div>
       </div>
     </div>
+
+    <div class="container">
+      <div class="row section-break">
+        <div class="col-sm-12 col-lg-4">
+          <img class="xl-streamline-illustration center mt2 mb2" src="http://localhost:8888/gnp/wp-content/uploads/2018/09/37-alarm-clock.svg" alt="alarm clock wake up illustration">
+        </div>
+        <div class="col-sm-12 col-lg-8">
+           <h2 class="mb1">It's Time For A Wake Up Call</h2>
+           <p class="lead">Most people are really bad at setting personal goals.</p>
+           <p>Good intentions don't deliver results. If you aren't following a proven planning process you are setting yourself up for failure. <a href="/resources/5fs">Start here</a>. Try Growth & Purpose's free 5F planning resource to get you on the right track towards accomplishing your personal goals.</p>
+           <blockquote>Scientific studies suggest only 8% of people actually achieve their goals. <cite><a href="https://www.inc.com/marcel-schwantes/science-says-only-8-percent-of-people-actually-achieve-their-goals-here-are-7-things-they-do-differently.html">Source</a></cite></blockquote>
+          </div>
+       </div>
+     </div>
+     <section class="section-light">
+       <div class="container">
+         <div class="row section-break">
+           <div class="col-sm-12 col-lg-8">
+             <h2 class="mb1">Find More Accountability For Your Personal Goals</h2>
+             <p class="lead">Even if you have a perfectly executed goal you will still fail without accountability.</p>
+             <p>Who or what are you accountable for in your life? If you aren't sure then find someone who has invested their time and energy in you and wants to see you succeed. Use them to check in and help you to get unstuck when you are not progressing towards your goal.</p>
+             <a class="btn btn-lg btn-success" href="/resources/">Need An Accountability Partner?</a>
+            </div>
+           <div class="col-sm-12 col-lg-4">
+             <img class="xl-streamline-illustration center mt2" src="https://growthandpurpose.com/wp-content/uploads/18-business-target-2.svg" alt="hit your goals illustration">
+          </div>
+         </div>
+       </div>
+    </section>
+    <div class="container">
+      <div class="row section-break">
+        <h2 class="center mb1">See What Others Are Saying</h2>
+        <div class="col-sm-12 col-lg-4">
+          <blockquote>Alex is awesome. I really appreciate him helping me reach my fitness goal in 24 days. It was amazing having him invest in me and help me stay on track. I highly recommend working with him. <cite> <img class="img-circle" width="40" src="https://randomuser.me/api/portraits/men/65.jpg" /> Tom Smith</cite> </blockquote>
+        </div>
+        <div class="col-sm-12 col-lg-4">
+          <blockquote>Alex is awesome. I really appreciate him helping me reach my fitness goal in 24 days. It was amazing having him invest in me and help me stay on track. I highly recommend working with him. <cite> <img class="img-circle" width="40" src="https://randomuser.me/api/portraits/men/65.jpg" /> Tom Smith</cite></blockquote>
+        </div>
+        <div class="col-sm-12 col-lg-4">
+          <blockquote>Alex is awesome. I really appreciate him helping me reach my fitness goal in 24 days. It was amazing having him invest in me and help me stay on track. I highly recommend working with him. <cite> <img class="img-circle" width="40" src="https://randomuser.me/api/portraits/men/65.jpg" /> Tom Smith</cite></blockquote>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row section-break">
+        <h2 class="center mb1">How Does It Work?</h2>
+        <div class="col-sm-12 col-lg-4">
+          <img class="sm-streamline-illustration center" src="http://localhost:8888/gnp/wp-content/uploads/2018/09/37-alarm-clock.svg" alt="alarm clock wake up illustration">
+          <h3>1. Fill Out An Application</h3>
+          <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum </p>
+        </div>
+        <div class="col-sm-12 col-lg-4">
+          <img class="sm-streamline-illustration center" src="http://localhost:8888/gnp/wp-content/uploads/2018/09/37-alarm-clock.svg" alt="alarm clock wake up illustration">
+          <h3>1. Fill Out An Application</h3>
+          <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum </p>
+        </div>
+        <div class="col-sm-12 col-lg-4">
+          <img class="sm-streamline-illustration center" src="http://localhost:8888/gnp/wp-content/uploads/2018/09/37-alarm-clock.svg" alt="alarm clock wake up illustration">
+          <h3>1. Fill Out An Application</h3>
+          <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum </p>
+        </div>
+     </div>
+     <div class="row">
+       <div class="col-sm-12 col-lg-8 col-lg-offset-2">
+          <?php get_template_part('templates/content', 'page'); ?>
+         </div>
+      </div>
+   </div>
     <?php } ?>
     <div class="wrap container" role="document">
       <div class="content row">
--- wp-content/themes/sage/template-home.php
+++ wp-content/themes/sage/template-home.php
@@ -6,33 +6,5 @@
 
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
-  <div class="container">
-  <div class="row section-break">
-    <div class="col-sm-12 col-lg-6">
-      <img class="xl-streamline-illustration center mt2 mb2" src="https://growthandpurpose.com/wp-content/uploads/illustration@2x.png" alt="launch idea illustration">
-    </div>
-    <div class="col-sm-12 col-lg-6">
-       <h2 class="mb1">Tired Of Failing To Reach Your Goals?</h2>
-       <p class="lead">Many people will set goals with all the intention in the world to follow through and reach them.</p>
-       <p>But less than a week in and they are already failing. That is where accountability comes in. What are you currently doing to hold yourself accountable to reaching your goals? If the answer is nothing, have you considered an accounability partner?<a href="/what-is-an-accountability-partner"> Read more about what an accountability partner is.</a></p>
-       <blockquote>Science Says Only 8 Percent of People Actually Achieve Their Goals. <cite><a href="https://www.inc.com/marcel-schwantes/science-says-only-8-percent-of-people-actually-achieve-their-goals-here-are-7-things-they-do-differently.html">Source</a></cite></blockquote>
-      </div>
-   </div>
-   <div class="row section-break">
-     <div class="col-sm-12 col-lg-6">
-       <h2 class="mb1">Find More Accountability For Your Actions</h2>
-       <p class="lead">Sometimes all it takes to reach your goal is to have an accountability partner.</p>
-       <p>Find someone who is invested and wants to see you succeed. Use them to check in and help you get unstuck when you are not progressing towards your goal.</p>
-       <a class="btn btn-lg btn-success" href="/resources/">Get Started</a>
-      </div>
-     <div class="col-sm-12 col-lg-6">
-       <img class="xl-streamline-illustration center mt2" src="https://growthandpurpose.com/wp-content/uploads/07-Hand-Shaking.png" alt="drowning illustration">
-    </div>
-   </div>
-   <div class="row">
-     <div class="col-sm-12 col-lg-8 col-lg-offset-2">
-        <?php get_template_part('templates/content', 'page'); ?>
-       </div>
-    </div>
- </div>
+
 <?php endwhile; ?>
