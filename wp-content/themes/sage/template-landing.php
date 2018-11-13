7fad51a8a31e28ed343454b2f81835372b964e98
Alex Zerbach <ayezee33@gmail.com>
2018-11-12 19:32:02 -0800

    button and form updates

--- wp-content/themes/sage/template-landing.php
+++ wp-content/themes/sage/template-landing.php
@@ -5,6 +5,12 @@
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+  <div class="container">
+  <div class="row">
+     <div class="col-sm-12 col-md-12 col-lg-8 col-lg-offset-2">
+       <?php get_template_part('templates/page', 'header'); ?>
+       <?php get_template_part('templates/content', 'page'); ?>
+      </div>
+   </div>
+ </div>
 <?php endwhile; ?>
