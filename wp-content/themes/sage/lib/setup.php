9112369e4c2e4f88129f65c70e1f64e5a827bdc4
Alex Zerbach <ayezee33@gmail.com>
2018-11-16 07:36:11 -0800

    Changes

--- wp-content/themes/sage/lib/setup.php
+++ wp-content/themes/sage/lib/setup.php
@@ -92,14 +92,13 @@ function display_sidebar() {
     // The sidebar will NOT be displayed if ANY of the following return true.
     // @link https://codex.wordpress.org/Conditional_Tags
     is_404(),
-    is_page_template('template-custom.php'),
     is_page_template('template-landing.php'),
     is_page_template('template-home.php'),
     is_page_template('template-acct-partner.php'),
+    is_archive('resources'),
     is_single(),
     is_home(),
-    is_front_page(),
-    is_archive('resources'),
+    is_front_page()
   ]);
 
   return apply_filters('sage/display_sidebar', $display);
